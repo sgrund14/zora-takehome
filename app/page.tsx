755063895c76e70c9d3f339639ff7c45858ea07b
sam <sgrund626@gmail.com>
2024-05-13 19:04:01 -0400

    feat: zora take home test

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,253 @@
-import Image from "next/image";
+"use client";
+
+import { useState, useCallback } from "react";
+
+type Photo = {
+  id: string;
+  urls: { regular: string };
+  color: string | null;
+  description: string;
+};
+
+const colors = [
+  "black_and_white",
+  "black",
+  "white",
+  "yellow",
+  "orange",
+  "red",
+  "purple",
+  "magenta",
+  "green",
+  "teal",
+  "blue",
+];
+
+// my take home test :)
+// cleanup ideas:
+// - refactor into smaller components for better legibility (Search, Filters, Results, Pagination)
+// - improve loading placeholder so that there is less layout shift
+// - move unsplash api search to server if you wanted to mask the api key
+// - add error handling (some red text or something)
 
 export default function Home() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [shouldFetchLatest, setShouldFetchLatest] = useState(false);
+  const [selectedColor, setSelectedColor] = useState<string | undefined>();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [results, setResults] = useState<Photo[] | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchImages = useCallback(
+    async ({
+      query,
+      color,
+      orderBy,
+      page,
+    }: {
+      query: string;
+      page: number;
+      color?: string;
+      orderBy?: "latest";
+    }) => {
+      setResults(null);
+      setIsLoading(true);
+      setTotalPages(1);
+      setCurrentPage(page);
+      const queryParams = new URLSearchParams({
+        client_id: "ukJyK7f_oSD8sRs6GytnZxaxCnv8XCiFi05QKrF_BeQ",
+        page: `${page}`,
+        per_page: "9",
+      });
+      queryParams.set("query", query);
+      if (color) {
+        queryParams.set("color", color);
+      }
+      if (orderBy) {
+        queryParams.set("order_by", orderBy);
+      }
+      try {
+        const res = await fetch(
+          `https://api.unsplash.com/search/photos?${queryParams.toString()}`
+        );
+        const data = await res.json();
+        if (data.errors) {
+          setResults([]);
+          setTotalPages(1);
+        } else {
+          const totalPages = data.total_pages;
+          const photos = data.results as Photo[];
+          setResults(photos);
+          setTotalPages(totalPages);
+        }
+        setIsLoading(false);
+      } catch {
+        setResults([]);
+        setTotalPages(1);
+      }
+    },
+    []
+  );
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <main className="flex min-h-screen flex-col items-center py-24 gap-5">
+      {/* SEARCH INPUT */}
+      <div className="flex flex-col sm:flex-row items-center gap-2">
+        <input
+          placeholder="Search"
+          className="rounded-xl bg-gray-100 text-center py-2"
+          value={searchTerm}
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" && searchTerm) {
+              fetchImages({
+                query: searchTerm,
+                color: selectedColor,
+                orderBy: shouldFetchLatest ? "latest" : undefined,
+                page: 1,
+              });
+            }
+          }}
         />
+        <button
+          disabled={!searchTerm}
+          onClick={() => {
+            fetchImages({
+              query: searchTerm,
+              color: selectedColor,
+              orderBy: shouldFetchLatest ? "latest" : undefined,
+              page: 1,
+            });
+          }}
+          className="h-[40px] disabled:opacity-50 disabled:cursor-not-allowed rounded-xl bg-blue-600 text-white px-4 py-[2px]"
+        >
+          Search
+        </button>
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      {/* SORT AND FILTER TOOLBAR */}
+      <div className="flex px-4 py-2 rounded-xl bg-gray-100 gap-3 max-w-[100vw] overflow-auto">
+        <span className="flex gap-2 items-center shrink-0">
+          <input
+            type="checkbox"
+            id="latest"
+            name="latest"
+            value="Latest"
+            className="cursor-pointer"
+            checked={shouldFetchLatest}
+            onChange={(e) => {
+              setShouldFetchLatest(e.target.checked);
+              // only trigger search when there is a search term
+              if (searchTerm) {
+                fetchImages({
+                  query: searchTerm,
+                  color: selectedColor,
+                  orderBy: e.target.checked ? "latest" : undefined,
+                  page: 1,
+                });
+              }
+            }}
+          />
+          <label htmlFor="latest" className="cursor-pointer">
+            Sort by Latest
+          </label>
+        </span>
+        {colors.map((color) => {
+          const isSelectedColor = color === selectedColor;
+          return (
+            <button
+              key={color}
+              className={`shrink-0 bg-white rounded-xl px-2 py-[2px] ${
+                isSelectedColor ? "outline outline-blue-600" : ""
+              }`}
+              onClick={() => {
+                const newColor = isSelectedColor ? undefined : color;
+                setSelectedColor(newColor);
+                // only trigger search when there is a search term
+                if (searchTerm) {
+                  fetchImages({
+                    query: searchTerm,
+                    color: newColor,
+                    orderBy: shouldFetchLatest ? "latest" : undefined,
+                    page: 1,
+                  });
+                }
+              }}
+            >
+              {color}
+            </button>
+          );
+        })}
       </div>
+
+      {/* RESULTS */}
+      {results && !!results.length && (
+        <div className="grid grid-cols-2 sm:grid-cols-3 gap-3 px-5">
+          {results.map((res) => {
+            return (
+              <div
+                key={res.id}
+                className="aspect-square rounded-xl overflow-hidden"
+              >
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img
+                  alt={res.description}
+                  src={res.urls.regular}
+                  className="object-cover h-full w-full"
+                />
+              </div>
+            );
+          })}
+        </div>
+      )}
+
+      {!isLoading && searchTerm && results && !results.length && (
+        <span>No results found</span>
+      )}
+      {!isLoading && !searchTerm && !results && (
+        <span>Enter search term to find images 😊</span>
+      )}
+      {isLoading && <span>Loading...</span>}
+
+      {/* PAGINATION */}
+      {totalPages > 1 && (
+        <div className="flex flex-col items-center gap-2">
+          <div className="flex gap-5">
+            <button
+              disabled={currentPage === 1}
+              onClick={() => {
+                fetchImages({
+                  query: searchTerm,
+                  color: selectedColor,
+                  orderBy: shouldFetchLatest ? "latest" : undefined,
+                  page: currentPage - 1,
+                });
+              }}
+              className="disabled:opacity-50 disabled:cursor-not-allowed rounded-xl bg-blue-600 text-white px-4 py-[2px]"
+            >
+              Prev
+            </button>
+            <button
+              disabled={currentPage === totalPages}
+              onClick={() => {
+                fetchImages({
+                  query: searchTerm,
+                  color: selectedColor,
+                  orderBy: shouldFetchLatest ? "latest" : undefined,
+                  page: currentPage + 1,
+                });
+              }}
+              className="disabled:opacity-50 disabled:cursor-not-allowed rounded-xl bg-blue-600 text-white px-4 py-[2px]"
+            >
+              Next
+            </button>
+          </div>
+          <span>{`${currentPage} / ${totalPages}`}</span>
+        </div>
+      )}
     </main>
   );
 }
